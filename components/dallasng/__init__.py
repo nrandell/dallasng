c99c4d24702abd7c2345b1b1e997e271f4330f51
nrandell <nick@nickrandell.com>
2022-11-20 14:06:05 +0000

    Upgrade one wire package version to fix issue error #1

--- components/dallasng/__init__.py
+++ components/dallasng/__init__.py
@@ -20,7 +20,7 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    cg.add_library("https://github.com/pstolarz/OneWireNg", "0.12.0")
+    cg.add_library("https://github.com/pstolarz/OneWireNg", "0.12.1")
 
     pin = await cg.gpio_pin_expression(config[CONF_PIN])
     cg.add(var.set_pin(pin))
